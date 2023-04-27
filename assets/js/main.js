f3030749ff9016e989236bc0ef8d92c1bad0c7af
zeitlosein <nikolassmith21@gmail.com>
2023-04-27 04:10:11 -0400

    tons changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,17 @@ document.querySelectorAll('nav ul li a').forEach(link => {
     });
   });
   
-  const imageNames = ['warehouse', 'lumping', 'check'];
-  let currentImageIndex = 0;
+  // const imageNames = ['warehouse', 'lumping', 'check'];
+  // let currentImageIndex = 0;
   
-  setInterval(() => {
-    const sections = document.querySelectorAll('.bg-image');
-    currentImageIndex++;
-    if (currentImageIndex >= imageNames.length) currentImageIndex = 0;
+  // setInterval(() => {
+  //   const sections = document.querySelectorAll('.bg-image');
+  //   currentImageIndex++;
+  //   if (currentImageIndex >= imageNames.length) currentImageIndex = 0;
   
-    sections.forEach((section, index) => {
-      const nextImageIndex = (index + currentImageIndex) % imageNames.length;
-      section.style.backgroundImage = `url('/assets/images/${imageNames[nextImageIndex]}.jpg')`;
-    });
-  }, 10000);
+  //   sections.forEach((section, index) => {
+  //     const nextImageIndex = (index + currentImageIndex) % imageNames.length;
+  //     section.style.backgroundImage = `url('/assets/images/${imageNames[nextImageIndex]}.jpg')`;
+  //   });
+  // }, 10000);
   
